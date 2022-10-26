61bfb43cc9849236e3a762d0a5b4e1d5d97d571f
陳彥瑋 <wayne9785@gmail.com>
2022-10-26 17:26:28 +0800

    更新版

--- database/seeders/DriversTableSeeder.php
+++ database/seeders/DriversTableSeeder.php
@@ -12,16 +12,49 @@ class DriversTableSeeder extends Seeder
      *
      * @return void
      */
+    public function genrateRandomString($length = 10){
+        $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;                
+    }
+    public function generateRandomName() {
+        $first_name = $this->genrateRandomString(rand(2, 15));
+        $first_name = strtolower($first_name);
+        $first_name = ucfirst($first_name);
+        $last_name = $this->genrateRandomString(rand(2, 15));
+        $last_name = strtolower($last_name);
+        $last_name = ucfirst($last_name);
+        $name = $first_name . " ". $last_name;
+        return $name;
+    }
+    public function generateRandomcountryofbirth() {
+        $positions = ['德國', '加拿大', '芬蘭', '西班牙', '法國', '中國', '丹麥', '瑞士', '澳洲','比利時','墨西哥','摩納哥','英國','日本'];
+        return $positions[rand(0, count($positions)-1)];
+
+    }
     public function run()
     {
-        DB::table('drivers')->insert([
-            'name' => "黃福誠",
-            'tid' => 2,
-            'number' => 16,
-            'frequency' => 1,
-            'integral' => 2.3,
-            'birthday' => "2002-01-16",
-            'countryofbirth' => "印度",          
-        ]);
+        for ($i=0; $i<22; $i++)
+        {
+            $name = $this->generateRandomName();
+            $countryofbirth = $this->generateRandomcountryofbirth();
+            $random_datetime = Carbon::now()->subMinutes(rand(1, 55));
+            $birthday = Carbon::now()->subYears(rand(48, 60))->subMonths(rand(0, 12))->subRealDays(rand(0,31));
+                DB::table('drivers')->insert([
+                'name' => $name,
+                'tid' => rand(1, 10),
+                'birthday' => $birthday,
+                'countryofbirth' => $countryofbirth,
+                'number' => rand(1, 77),
+                'frequency' => rand(17, 353),
+                'integral' => rand(0, 10)/10 * 4335,
+                'created_at' => $random_datetime,
+                'updated_at' => $random_datetime
+            ]);
+        }
     }
 }
--- database/seeders/FleetsTableSeeder.php
+++ database/seeders/FleetsTableSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace Database\Seeders;
-
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -12,8 +12,52 @@ class FleetsTableSeeder extends Seeder
      *
      * @return void
      */
+    public function generateRandomString($length = 10) {
+        $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+    public function generateRandomName() {
+        $first_name = $this->generateRandomString(rand(2, 15));
+        $first_name = strtolower($first_name);
+        $first_name = ucfirst($first_name);
+        $last_name = $this->generateRandomString(rand(2, 15));
+        $last_name = strtolower($last_name);
+        $last_name = ucfirst($last_name);
+        $name = $first_name . " ". $last_name;
+        return $name;
+    }
+    public function generateRandomcountry() {
+        $country = ['義大利',  '法國', '美國', '德國', '奧地利','英國','瑞士'];
+        return $country[rand(0, count($country)-1)];
+
+    }
+    public function generateRandomlocation() {
+        $location = ['義大利瑪拉內羅',  '義大利法恩札', '美國北卡羅萊納州坎納波里利斯', '英國北安普敦郡', '英國薩里郡沃金','英國銀石','瑞士蘇黎世州欣維爾','英國牛津郡恩斯頓','英國牛津郡格羅夫','英國白金漢郡米爾頓凱恩斯'];
+        return $location[rand(0, count($location)-1)];
+
+    }
     public function run()
     {
-        //
+        for ($i=0; $i<10; $i++) {
+            $name = $this->generateRandomName();
+            $country = $this->generateRandomcountry();
+            $location = $this->generateRandomlocation();
+            
+            $random_datetime = Carbon::now()->subMinutes(rand(1, 55));
+
+            DB::table('fleets')->insert([
+                'name' => $name,
+                'country' => $country,
+                'location' => $location,
+                
+                'created_at' => $random_datetime,
+                'updated_at' => $random_datetime
+            ]);
+        }
     }
 }
